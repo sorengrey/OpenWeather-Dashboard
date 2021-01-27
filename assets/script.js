6d359c78924ad69dd19d09d595c5b5ca8e0e9551
sorengrey <65551656+sorengrey@users.noreply.github.com>
2021-01-27 15:17:36 -0500

    Searched cities save to local storage, history list

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
   // search button functionality
   $("#searchbutton").on("click", function SearchForCity(input){
     var input = $("#search").val();
-
+    //set the searched city local storage
+    localStorage.setItem("city:", input);
+    // ajax call that gets city info
     $.ajax({
      type: "GET",
      url: "http://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=77d824887f06ac6836449d9d10feb418&units=imperial",
@@ -11,5 +13,11 @@ $(document).ready(function() {
   }).then(function(response) {
     console.log(response);
 })
+function addCities(text) {
+  var searchedCity = $("<li>").addClass("list-group-item").text(text);
+  $(".historylist").append(searchedCity);
+}
+addCities(input);
   })
+
 })
