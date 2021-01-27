34d1e42a0f99b815a66b0c6a4b94dde61ee67946
sorengrey <65551656+sorengrey@users.noreply.github.com>
2021-01-27 12:28:05 -0500

    Added ajax call for city search

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,15 @@
+$(document).ready(function() {
 
-// this will be part of the submit button 
-$("a").click(function(event){
-  event.preventDefault();
-});
+  // search button functionality
+  $("#searchbutton").on("click", function SearchForCity(input){
+    var input = $("#search").val();
+
+    $.ajax({
+     type: "GET",
+     url: "http://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=77d824887f06ac6836449d9d10feb418&units=imperial",
+     dataType: "json"
+  }).then(function(response) {
+    console.log(response);
+})
+  })
+})
