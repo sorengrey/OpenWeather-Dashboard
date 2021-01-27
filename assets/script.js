fed1206a75e8354818c737f9b86ca88cdb11fcb1
sorengrey <65551656+sorengrey@users.noreply.github.com>
2021-01-27 17:03:50 -0500

    Added card for current city's weather
    Card displays city name, humidity, wind speed. Needs date and UV index now.

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,38 @@
 $(document).ready(function() {
 
   // search button functionality
-  $("#searchbutton").on("click", function SearchForCity(input){
+  $("#searchbutton").on("click", function(input){
+    // sets what the user searched for as a variable
     var input = $("#search").val();
-    //set the searched city local storage
+    // saves the searched-for city in local storage
     localStorage.setItem("city:", input);
-    // ajax call that gets city info
+
+    // ajax call that gets city's weather info
     $.ajax({
      type: "GET",
      url: "http://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=77d824887f06ac6836449d9d10feb418&units=imperial",
-     dataType: "json"
-  }).then(function(response) {
-    console.log(response);
-})
+     dataType: "json",
+     success: (function(data){
+    // test
+    console.log(data);
+    // variables for the current weather conditions - uv will have to be separate
+      var currentCity = $("<h2>").addClass("card-current").text(data.name);
+      var currentTemp = $("<p>").text("Temperature: " + data.main.temp + "°F");
+      var currentHumid = $("<p>").text("Humidity: " + data.main.humidity + "%");
+      var currentWind = $("<p>").text("Wind Speed: " + data.wind.speed + "MPH");
+      var cardInfo = $("<div>").addClass("card-body");
+      var addCard = $("<div>").addClass("card");
+      cardInfo.append(currentCity, currentTemp, currentHumid, currentWind);
+      addCard.append(cardInfo);
+      $(".currentday").append(addCard);
+    })
+  })
+// creates list elements and adds searched-for cities to the history list
 function addCities(text) {
   var searchedCity = $("<li>").addClass("list-group-item").text(text);
   $(".historylist").append(searchedCity);
 }
 addCities(input);
-  })
 
 })
+  })
